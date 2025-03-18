cb141a66d38253a37a06c5b81ef468fcaea22baf
hemant marothi <hemant.marothi@metacube.com>
2025-03-18 13:24:26 +0530

    Comments added

--- Software Construction Fundamentals/Assignment 8/EmployeeManagementSystem.java
+++ Software Construction Fundamentals/Assignment 8/EmployeeManagementSystem.java
@@ -8,28 +8,67 @@ public class EmployeeManagementSystem {
     private static final Scanner sc = new Scanner(System.in);
     private static final Set<Integer> employeeIds = new HashSet<>();
 
-    private static void addDepartment() {
+    /**
+     * Adds a new department to the oraganization.
+     * @return true if successfully added else false. 
+     */
+    private static boolean addDepartment() {
         System.out.print("Enter Department Name: ");
         String name = sc.nextLine().trim();
 
         if (findDepartment(name) != null) {
             System.out.println("Error: Department already exists!");
-            return;
+            return false;
         }
 
         Department dept = new Department(name);
         organization.addDepartment(dept);
         System.out.println("Department added successfully!");
+        return true;
     }
 
-    private static void addEmployee() {
+    /**
+     * Relieves or removes an employee if present in the organisation based on ID.
+     * @return true if removed else false.
+     */
+    private static boolean removeEmployee() {
+        int id;
+        System.out.print("Enter Employee ID: ");
+        id = getIntInput(sc, 1, Integer.MAX_VALUE);
+
+        if (!employeeIds.contains(id)) {
+            System.out.println("Error: Employee ID doesn't exists!");
+            return false;
+        }
+        Employee employeeToRemove = null;
+        Department departmentOfEmployee = null;
+        for(Department dep : organization.departments){
+            for(Employee employee : dep.employees){
+                if(employee.getId() == id){
+                    departmentOfEmployee = dep;
+                    employeeToRemove = employee;
+                    break;
+                }
+            }
+        }
+        departmentOfEmployee.relieve(employeeToRemove);
+        employeeIds.remove(id);
+        System.out.println("Employee removed successfully!");
+        return true;
+    }
+
+    /**
+     * Adds an employee to a specific department under the organisation.
+     * @return true if added else false.
+     */
+    private static boolean  addEmployee() {
         System.out.print("Enter Department Name: ");
         String deptName = sc.nextLine().trim();
         Department department = findDepartment(deptName);
 
         if (department == null) {
             System.out.println("Error: Department not found! Please add the department first.");
-            return;
+            return false;
         }
 
         System.out.print("Enter Employee Name: ");
@@ -61,8 +100,12 @@ public class EmployeeManagementSystem {
         department.join(employee);
         employeeIds.add(id);
         System.out.println("Employee added successfully!");
+        return true;
     }
 
+    /**
+     * Prints all Existing employees of an organisation.
+     */
     private static void viewEmployees() {
         List<Employee> allEmployees = organization.getAllEmployees();
         if (allEmployees.isEmpty()) {
@@ -79,6 +122,11 @@ public class EmployeeManagementSystem {
         }
     }
 
+    /**
+     * Checks if department exists or not in the organisation and if exists return its object.
+     * @param name of the department.
+     * @return object of department if exists else null.
+     */
     private static Department findDepartment(String name) {
         for (Department dept : organization.departments) {
             if (dept.getName().equalsIgnoreCase(name)) {
@@ -127,7 +175,6 @@ public class EmployeeManagementSystem {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         while (true) {
             try{
                 System.out.println("\n======= Welcome to employee management system =======");
@@ -135,11 +182,12 @@ public class EmployeeManagementSystem {
                 System.out.println("2. Add employee.");
                 System.out.println("3. View all employees.");
                 System.out.println("4. Print salary slips.");
-                System.out.println("5. Exit.");
+                System.out.println("5. Remove an employee.");
+                System.out.println("6. Exit.");
                 System.out.println("=====================================================");
 
                 System.out.print("Enter your choice: ");
-                int choice = getIntInput(sc, 1, 5);
+                int choice = getIntInput(sc, 1, 6);
                 switch (choice) {
                     case 1:
                         addDepartment();
@@ -154,6 +202,9 @@ public class EmployeeManagementSystem {
                         Payroll.printSalarySlips(organization);
                         break;
                     case 5:
+                        removeEmployee();
+                        break;
+                    case 6:
                         return;
                 }
             }catch(Exception e){
--- Software Construction Fundamentals/Assignment 8/Payroll.java
+++ Software Construction Fundamentals/Assignment 8/Payroll.java
@@ -1,5 +1,10 @@
 
 public class Payroll {
+    /**
+     * Calculates tax based on the salary slab.
+     * @param salary
+     * @return calculated tax.
+     */
     public static double calculateTax(double salary){
         if(salary <= 25000){
             return salary * 0.1;
@@ -9,15 +14,23 @@ public class Payroll {
         return salary * 0.3;
     }
 
+    /**
+     * Prints all the salary and paroll details of the organization.
+     * @param organization
+     */
     public static void printSalarySlips(Organization organization){
-        System.out.println("+-----------------------------------------------------------------------+");
-        System.out.println("|  Payroll Summary for Organization: " + organization.getName() + "                           |");
-        System.out.println("+-----------------------------------------------------------------------+");
-        System.out.println("|  EMP_ID  |     EMP_NAME     | BASIC_SALARY |   BONUS   | COMPENSATION |");
-        for(Employee employee : organization.getAllEmployees()){
-            System.out.printf("| %-8d | %-16s | %-12.2f | %-9.2f | %-12.2f |\n", employee.getId(),
-            employee.getName(), employee.getBasicSalary(), employee.getBonus(), employee.getCompensation());
+        if(organization.getAllEmployees().isEmpty()){
+            System.out.println("No Employees found !!");
+            return;
         }
-        System.out.println("\n+----------------------------------------------------------------------+");
+        System.out.println("+----------------------------------------------------------------------------------+");
+        System.out.println("|  Payroll Summary for Organization: " + organization.getName() + "                                      |");
+        System.out.println("+----------------------------------------------------------------------------------+");
+        System.out.println("|  EMP_ID  |     EMP_NAME     | BASIC_SALARY |   BONUS   | COMPENSATION | TAX_RATE |");
+        for(Employee employee : organization.getAllEmployees()){
+            System.out.printf("| %-8d | %-16s | %-12.2f | %-9.2f | %-12.2f | %-8.2f\n", employee.getId(),
+            employee.getName(), employee.getBasicSalary(), employee.getBonus(), employee.getCompensation(), calculateTax(employee.getBasicSalary()));
+        }
+        System.out.println("+----------------------------------------------------------------------------------+");
     }
 }
