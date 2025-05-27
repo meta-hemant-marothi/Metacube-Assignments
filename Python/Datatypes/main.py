6bdc9b2aa70b1565435f47b4ac4d50569bbda287
hemant marothi <hemant.marothi@metacube.com>
2025-05-27 14:10:19 +0530

    Assignment completed

---
+++ Python/Datatypes/main.py
@@ -0,0 +1,6 @@
+integer_value = 10
+floating_value = 20.54
+boolean_value = True
+string_value = "Python is Awesome"
+
+print(f"Integer Value: {integer_value} \nFloating Value: {floating_value} \nBoolean Value: {boolean_value} \nString Value: {string_value}")
