5e76e651b7386fbfa9f12cee5d64a9bd0e30d391
hemant marothi <hemant.marothi@metacube.com>
2025-05-27 14:25:06 +0530

    assignemnt complete

---
+++ Python/Sequence Types/main.py
@@ -0,0 +1,22 @@
+# Create a list of three countries. 
+countries = ["India", "Japan", "Russia"]
+
+# Add a country at the end of the list.
+countries.append("China")
+
+# Remove any country using its index.
+countries.pop(2)
+
+# Add a country in the middle of the list.
+countries.insert(2, "Russia")
+
+
+
+# Create a set of three countries. 
+countries = {"India", "Japan", "Russia"}
+
+# Add a country at the end of the set.
+countries.add("China")
+
+# Remove any country using its index.
+countries.pop()
