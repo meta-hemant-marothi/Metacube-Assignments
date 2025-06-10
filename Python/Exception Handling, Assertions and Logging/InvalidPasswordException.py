801453be0412779316229e858c4b50ff82c691c6
hemant marothi <hemant.marothi@metacube.com>
2025-06-10 12:34:53 +0530

    assignment completed

---
+++ Python/Exception Handling, Assertions and Logging/InvalidPasswordException.py
@@ -0,0 +1,20 @@
+class InvalidPasswordException(Exception):
+    """ Error is raised when the length of password is less than 8 characters
+        Attributes:
+            salary -- input salary which caused the error
+            message -- explanation of the error
+    """
+
+    def __init__(self, password):
+        self.password = password
+        self.message = "The length of password is less than 8 characters"
+        super().__init__(self.message)
+
+
+try:
+    password = input("Enter your password: ")
+    if len(password) < 8:
+        raise InvalidPasswordException(password)
+
+except InvalidPasswordException as e:
+    print(e)
